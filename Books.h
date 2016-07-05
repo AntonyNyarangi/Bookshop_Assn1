75b36b656fcaac76571d7ad0a858576b93241277
AntonyNyarangi <antony.momanyi@gmail.com>
2016-07-05 10:13:16 +0300

    Add files via upload

---
+++ Books.h
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// defining the Books class
+class Books{
+public:
+  //public data members
+  static int successfulTransactions;
+  static int unsuccessfulTransactions;
+  string authorName;
+  string bookTitle;
+  string bookPublisher;
+  double bookPrice;
+  int stockValue;
+
+  //class constructor
+  Books(string bTitle, string aName, string bPublisher, double bPrice, int stock){
+    bookTitle = bTitle;
+    authorName = aName;
+    bookPublisher = bPublisher;
+    bookPrice = bPrice;
+    stockValue = stock;
+    }
+    string getAuthorName(){
+      return authorName;
+  }
+
+  //method to display the details of a book object
+  void displayBookDetails (){
+    cout << "\n\nStatus=Available\nBook Details" << endl;
+    cout << "\tBook Title: " << bookTitle << endl;
+    cout << "\tAuthor Name: " << authorName << endl;
+    cout << "\tPublisher: " << bookPublisher << endl;
+    cout << "\tUnit Price: " << bookPrice << endl;
+    cout << "\tAvailable Stock: " << stockValue << endl;
+  }
+
+  //method to calculate the totsl cost of a book transaction given the number of Books needed
+  //also updates the book stockValue
+  double calculateTotalCost(int needed){
+    double cost;
+     if (needed > stockValue){
+       cout << "Required copies not in stock" << endl;
+     }else{
+       cost = needed * bookPrice;
+       stockValue = stockValue - needed;
+     }
+     return cost;
+   }
+   //method to record number of unsuccessful transactions
+   static void countUnsuccessfulTransactions (){
+     unsuccessfulTransactions = unsuccessfulTransactions + 1;
+     return;
+   }
+   //method to record number of successful transactions
+   static void coutSuccessfulTransactions(){
+     successfulTransactions = successfulTransactions + 1;
+     return;
+   }
+
+private:
+  //private member method
+  void updatePrice(double newPrice){
+    bookPrice = newPrice;
+  }
+};
